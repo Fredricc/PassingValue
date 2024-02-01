f323e6a995425eba72063eeae4468003662e8b97
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-02-01 03:40:04 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace PassingValue
+{
+    public struct Rectangle
+    {
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public double Width { get; set; }
+        public double Height { get; set; }
+    }
+
+    public class MethodParameters
+    {
+        
+        public static void ChangeHeight() 
+        { 
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+        
+                rectangle.Height = 500;
+                var rect = new Rectangle(200, 300);
+
+                Console.WriteLine($"rect height: {rect.Height}");
+
+
+                Console.WriteLine($"rectangle height in Method: {rectangle.Height}");
+            }
+    }
+}
